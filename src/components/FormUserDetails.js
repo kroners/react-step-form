3492307ef83b07f491e23562f4215d45f7d9612e
Carlos R <crodriguezq91@gmail.com>
2020-10-10 08:53:39 -0500

    Initial setup

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FormUserDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Initial Form View</h1>
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails
